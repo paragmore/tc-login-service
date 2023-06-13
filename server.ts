ae56507765af978f22cf004d106c3fd9420e0e8a
Parag More <paragmore306@gmail.com>
2023-06-13 10:32:51 +0530

    healthcheck api response change

--- server.ts
+++ server.ts
@@ -3,33 +3,42 @@ import Routes from "./router";
 import underPressure from "@fastify/under-pressure";
 import fastifySwagger from "@fastify/swagger";
 import cors from "@fastify/cors";
-import 'reflect-metadata';
+import "reflect-metadata";
 import fastifyStatic from "@fastify/static";
 import path from "path";
 import { connectMongoDB } from "./mongoose.config";
-import { config } from 'dotenv';
+import { config } from "dotenv";
 import { FastifyCookieOptions } from "@fastify/cookie";
-import cookie from '@fastify/cookie'
+import cookie from "@fastify/cookie";
 
 // Load environment variables from .env file
 config();
 const PORT = process.env.PORT || 8000;
-const app: FastifyInstance = fastify({ logger: true, disableRequestLogging: true });
-
+const HOST = process.env.HOST || "0.0.0.0";
+const app: FastifyInstance = fastify({
+  logger: true,
+  disableRequestLogging: true,
+});
 
 const underPressureConfig = () => {
   return {
     healthCheck: async function () {
       // TODO: Add database connection check
-      return true;
+      return { host: HOST, port: PORT, api: "login" };
     },
     message: "Under Pressure 😯",
-    exposeStatusRoute: "/status",
+    exposeStatusRoute: {
+      routeOpts: {},
+      routeResponseSchemaOpts: {
+        host: { type: "string" },
+        port: { type: "string" },
+        api: { type: "string" },
+      },
+      url: "/status", // If you also want to set a custom route path and pass options
+    },
     healthCheckInterval: 5000,
   };
 };
-
-
 
 const swaggerConfig = () => {
   return {
@@ -45,39 +54,39 @@ const swaggerConfig = () => {
       produces: ["application/json"],
     },
     exposeRoute: true,
-    port: process.env.PORT || 8000,
-    host: process.env.HOST || '0.0.0.0'
+    port: PORT,
+    host: HOST,
   };
 };
 
 const fastifyStaticConfig = () => {
-    console.log(path.join(__dirname, '../public'))
-    return {
-        root: path.join(__dirname, '../public'),
-        prefix: '/public/', // specify a prefix for your static file URLs
-      }
-}
-app.register(cors)
+  console.log(path.join(__dirname, "../public"));
+  return {
+    root: path.join(__dirname, "../public"),
+    prefix: "/public/", // specify a prefix for your static file URLs
+  };
+};
+app.register(cors);
 app.register(underPressure, underPressureConfig());
 app.register(fastifySwagger, swaggerConfig());
-app.register(fastifyStatic,fastifyStaticConfig())
+app.register(fastifyStatic, fastifyStaticConfig());
 app.register(cookie, {
   secret: "my-secret", // for cookies signature
-  hook: 'onRequest', 
-  parseOptions: {}     // options for parsing cookies
-} as FastifyCookieOptions)
-app.register(require('@fastify/swagger-ui'), {
-    routePrefix: '/documentation',
-    uiConfig: {
-      docExpansion: 'full',
-      deepLinking: false
-    },
-    staticCSP: true,
-    transformSpecificationClone: true
-  })
+  hook: "onRequest",
+  parseOptions: {}, // options for parsing cookies
+} as FastifyCookieOptions);
+app.register(require("@fastify/swagger-ui"), {
+  routePrefix: "/documentation",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  staticCSP: true,
+  transformSpecificationClone: true,
+});
 app.register(Routes);
 connectMongoDB();
-app.listen(PORT, '0.0.0.0' , (error, address) => {
+app.listen(PORT, "0.0.0.0", (error, address) => {
   if (error) {
     app.log.error(error);
     process.exit(1);
